e3377aa2059e5c6a78f00ba572245fdd4758c33b
Fanaina Alves <fanaiina.allves@gmail.com>
2024-08-02 17:32:07 -0300

    Conectando ao BD

--- src/main/java/io/github/fanainaalves/quarkussocial/rest/UserResource.java
+++ src/main/java/io/github/fanainaalves/quarkussocial/rest/UserResource.java
@@ -1,8 +1,33 @@
 package io.github.fanainaalves.quarkussocial.rest;
 
-import jakarta.ws.rs.Path;
+import io.github.fanainaalves.quarkussocial.domain.model.User;
+import io.github.fanainaalves.quarkussocial.rest.dto.CreateUserRequest;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
+
+    @POST
+    @Transactional
+    public Response createUser(CreateUserRequest userRequest){
+        User user = new User();
+        user.setAge(userRequest.getAge());
+        user.setName(userRequest.getName());
+
+        user.persist();
+
+        return Response.ok(user).build();
+    }
+
+    @GET
+    public Response listAllUsers(){
+        return Response.ok().build();
+    }
+
 
 }
